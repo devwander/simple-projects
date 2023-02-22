2e160ac76fbd9bac939570b1bc5f8ac90a14d683
devwander <josewanderson173@gmail.com>
2023-02-22 15:37:01 -0300

    Added project: Quiz with JavaScript

---
+++ quizApp-JavaScript/assets/js/questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        question: "Quanto é 3 + 2?",
+        answers: [
+            { option: "8", correct: false},
+            { option: "5", correct: true},
+            { option: "4", correct: false}
+        ]
+    },
+    {
+        question: "Quanto é 10 * 7?",
+        answers: [
+            { option: "50", correct: false},
+            { option: "60", correct: false},
+            { option: "70", correct: true}
+        ]
+    },
+    {
+        question: "Quanto é 5 * 2?",
+        answers: [
+            { option: "10", correct: true},
+            { option: "45", correct: false},
+            { option: "2", correct: false}
+        ]
+    },
+    {
+        question: "Quanto é 1 + 2?",
+        answers: [
+            { option: "8", correct: false},
+            { option: "3", correct: true},
+            { option: "2", correct: false}
+        ]
+    }
+]
