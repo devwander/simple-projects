69be3a2a31a75bdf79410f43f2b4dd4bf1225a86
Wanderson José <josewanderson173@gmail.com>
2023-04-03 22:20:11 -0300

    Added project: Music player with JavaScript

---
+++ music-player/songs.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        src: './assets/songs/floating-abstract-142819.mp3',
+        name: 'Floating Abstract',
+    },
+    {
+        src: './assets/songs/inspired-ambient-141686.mp3',
+        name: 'Inspired Ambient',
+    },
+    {
+        src: './assets/songs/inspiring-dream-140960.mp3',
+        name: 'Inspiring Dream',
+    },
+    {
+        src: './assets/songs/modern-vlog-140795.mp3',
+        name: 'Modern Vlog',
+    },
+    {
+        src: './assets/songs/risk-136788.mp3',
+        name: 'Risk',
+    },
+    {
+        src: './assets/songs/smoke-143172.mp3',
+        name: 'Smoke',
+    }
+]
