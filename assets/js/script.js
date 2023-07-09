16abbb6638310eeb9919d2d857d80b03bf1a22d7
devwander <josewanderson173@gmail.com>
2023-07-08 23:37:11 -0300

    refactor: change from flex display to grid and change in texts

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,29 @@ const emojis = [
 items.forEach((item, i) => {
     item.innerText = `${emojis[i]} <${item.innerText}/>`
 })
+
+const projects = [
+  "analog-clock/",
+  "consulting-weather-forecast/",
+  "convert-grid-to-csv/",
+  "dragInDrop-JavaScript/",
+  "menuFlutuante/",
+  "flying-dots-hover-effect/",
+  "gradientGenerator/",
+  "html-to-pdf/",
+  "carousel-of-images/",
+  "music-player/",
+  "passwordGenerator/",
+  "photo-editor/",
+  "qrCodeGenerator/",
+  "quizApp-JavaScript/",
+  "searchBar-JavaScript/",
+  "sending-email-form-html/",
+  "simple-accordion/",
+  "tags/",
+  "web-notes/"
+]
+
+const randomProject = () => {
+  window.location.href = projects[Math.floor(Math.random() * projects.length)];
+}
